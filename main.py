5e90acd73eb2e76394f388e8600e5d5de2797df0
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-07-26 09:35:34 +0200

    refactor: cleanup + window title

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 green = (0, 255, 0)
+
 WIDTH = 1000
 HEIGHT = 1000
+
 TIME_STEP_SIZE = 0.5
 
 class Flock(object):
@@ -14,7 +16,7 @@ class Flock(object):
     def __init__(self):
         pygame.init()
         self.display = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption('Marbles')
+        pygame.display.set_caption('Flock')
         self.clock = pygame.time.Clock()
         self.tick_time = time.time()
 
