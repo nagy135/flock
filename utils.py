a4284227ac0d3ed8f4b75cdac050ce6a9a5797ae
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-07-30 14:01:21 +0200

    feat: utils - average angle (vector version average)

--- utils.py
+++ utils.py
@@ -14,3 +14,35 @@ def rotate_point(centerPoint, point, angle):
 
 def euclidean_distance(x1, y1, x2, y2):
     return math.sqrt( (x1 - x2)**2 + (y1 - y2)**2 )
+
+def vector_average_angle(array_of_angles):
+    """
+    Calculates average between N angles
+
+    For 2 it works like this (generalized to more):
+    Calculated angle between 2 angles using vectors.
+    This means that it gets always closer angle to these 2 (not one on the other side)
+    10,30 => 20
+    0,90 => 45
+    0,180 => 90
+    270,360 => 315 (360-45)
+    """
+
+    # calculate array of cartesian tuples
+    cartesian_coordinates = [
+            (
+                math.cos(math.radians(a)),
+                math.sin(math.radians(a))
+                ) for a in array_of_angles
+            ]
+    average_x, average_y = 0,0
+    for (x,y) in cartesian_coordinates:
+        average_x += x
+        average_y += y
+    average_x /= len(cartesian_coordinates)
+    average_y /= len(cartesian_coordinates)
+
+    tangle = round(math.degrees(math.atan2(average_y, average_x)))
+    if tangle < 0:
+        return 360 + tangle
+    return tangle
